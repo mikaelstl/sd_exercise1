857233425ed24f18028ce4e2179f123df1ead68c
mikael <mikael.st15@protonmail.com>
2025-08-21 21:15:25 -0300

    refactor: changed Node project to Java project / added class to Mapper and Coordinator / init setup to Coordinator send messages to Mapper / setup containers into docker-compose file

---
+++ mapper/src/main/java/br/mikaelstl/mapreduce/App.java
@@ -0,0 +1,34 @@
+package br.mikaelstl.mapreduce;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPubSub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        String host = System.getenv().getOrDefault("REDIS_HOST", "localhost");
+        int port = Integer.parseInt(System.getenv().getOrDefault("REDIS_PORT", "6379"));
+        String mapperId = System.getenv().getOrDefault("MAPPER_ID", "0");
+        
+        final String channel = "mapper"+mapperId+"_stasks";
+
+        final Logger logger = LoggerFactory.getLogger("MAPPER"+mapperId);
+        
+        try (Jedis jedis = new Jedis(host, port);) {
+            jedis.subscribe(new JedisPubSub() {
+                @Override
+                public void onMessage(String channel, String message) {
+                    logger.info("Mapper " + mapperId + " recebeu tarefa: " + message);
+                }
+            }, channel);
+        }
+    }
+}
--- utils/fileSpliter.ts
+++
@@ -1,9 +0,0 @@
-/* class FileSpliter {
-  splitText(text: string): string[] {
-    return text
-      .toLowerCase()
-      .replace(/[^a-z0-9\s]/g, "")
-      .split(/\s+/)
-      .filter(Boolean);
-  }
-} */
